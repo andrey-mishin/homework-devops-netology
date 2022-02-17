c42a2c960406110438fd59d8d9e6cb80b9289a12
Andrey.Mishin <andrej.mishin@gmail.com>
2022-02-17 19:07:38 +0000

    4.2-python homework. 2nd & 3rd exercises fixed.

--- 4.2-python/ex2.py
+++ 4.2-python/ex2.py
@@ -4,9 +4,8 @@ import os
 
 bash_command = ["cd ~/.git/test-repo", "git status", "pwd"]
 result_os = os.popen(' && '.join(bash_command)).read()
-pwd = result_os.split('\n')
-print("Local repository is:", pwd[-2])
+pwd = result_os.split('\n')[-2]
 for result in result_os.split('\n'):				
     if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', 'modified file: ') 
-       	print(prepare_result)
+        prepare_result = result.replace('\tmodified:   ', '') 
+       	print(pwd + '/' + prepare_result)
--- 4.2-python/ex3.py
+++ 4.2-python/ex3.py
@@ -3,12 +3,15 @@
 import os
 import sys
 
-path = sys.argv[1]
-bash_command = ["cd {}".format(path), "git status", "pwd"]
-result_os = os.popen(' && '.join(bash_command)).read()
-pwd = result_os.split('\n')
-print("Local repository is:", pwd[-2])
-for result in result_os.split('\n'):				
+if len(sys.argv) > 1:
+  path = sys.argv[1]
+  bash_command = ["cd {}".format(path), "git status", "pwd"]
+  result_os = os.popen(' && '.join(bash_command)).read()
+  pwd = result_os.split('\n')[-2]
+  for result in result_os.split('\n'):				
     if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', 'modified file: ') 
-        print(prepare_result)
+      prepare_result = result.replace('\tmodified:   ', '') 
+      print(pwd + '/' + prepare_result)
+else:
+  print("Path is not defined!")
+  exit()
